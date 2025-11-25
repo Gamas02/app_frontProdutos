e2d8501123aedc7b9ecc07c824e6c25b4d81f7f6
Viganop <luccaperiotto@gmail.com>
2025-11-24 22:08:00 -0300

    New: Implementação do post categorias

---
+++ src/pages/categorias/Add.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+export default function Add() {
+  const [nome, setNome] = useState("");
+
+  const addCategorias = async (e) => {
+  e.preventDefault();
+
+  if (!nome.trim()) return;
+
+  try {
+    const res = await fetch("http://localhost:4567/categorias", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        nome,
+      }),
+    });
+
+    if (!res.ok) {
+      const txt = await res.text();
+      console.error("RESPOSTA DO BACKEND:", txt);
+      alert("Erro ao cadastrar produto. Veja o console.");
+      return;
+    }
+
+    setNome("");
+
+    alert("Produto cadastrado!");
+  } catch (error) {
+    console.error("Erro ao adicionar produto:", error);
+  }
+};
+
+
+  return (
+    <div className="page-container">
+      <h1 className="title">Cadastrar Produto</h1>
+
+      <form onSubmit={addProduto} className="form-card">
+
+        <div>
+          <label>Nome</label>
+          <input
+            className="input"
+            type="text"
+            placeholder="Digite nome da categoria"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </div>
+
+        <button className="btn create" type="submit">
+          Cadastrar
+        </button>
+      </form>
+    </div>
+  );
+}
